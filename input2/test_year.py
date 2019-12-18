9810f18273c096e84f09585464b369cda287c88d
thycan22 <thycan22@gmx.fr>
2019-12-18 16:31:04 +0100

    add day work

--- input2/test_year.py
+++ input2/test_year.py
@@ -1,6 +1,25 @@
+import unittest
 import year
 
+# test fonction year.py: validate_year()
+y = year.validate_year(2021)
+print(y)
 
-# test fonction year.py
 
-x = year.controller_input()
+class ValidateYearTestCase(unittest.TestCase):
+
+    def test(self):
+        y = year.validate_year('50')
+        self.assertEqual(y, 1950)
+        y = year.validate_year(1980)
+        self.assertEqual(y, 1980)
+        y = year.validate_year(13)
+        self.assertEqual(y, 2013)
+        y = year.validate_year(2021)
+        self.assertEqual(y, 'trop grand !')
+        y = year.validate_year('5jdm')
+        self.assertEqual(y, None)
+
+
+if __name__ == "__main__":
+    unittest.main()
--- input2/year.py
+++ input2/year.py
@@ -4,10 +4,8 @@ import datetime
 # traitement de l'année de naissance
 
 
-''' fonction controleur '''
-
-
 def controller_input():
+    ''' fonction controleur : insertion de l'utilisateur '''
     while True:
         ye = input(' Annee ? :')
 
@@ -15,14 +13,18 @@ def controller_input():
             print("Bye")
             break
         else:
+            ''' fonction qui valide la date'''
             y = validate_year(ye)
+            '''calcul de l'age et du nbre de jours '''
             dt = datetime.date.today()
-            print('année ', y)
-            print(datetime.date(y, 1, 1))
-            print(dt)
-            date_obj = datetime.date(y, 6, 14)
-            print(date_obj)
-            age = dt - date_obj
+            # print('année ', y)
+            # print(datetime.date(y, 1, 1))
+            # print(dt)
+            if isinstance(y, int):
+                date_obj = datetime.date(y, 6, 14)
+                # print(date_obj)
+                age = dt - date_obj
+            '''appel de l'affichage '''
             display_line_year(y, age.days)
 
 
@@ -30,10 +32,8 @@ def controller_file():
     pass
 
 
-''' fonction validation de l'année et calcul des ages et jours.'''
-
-
 def validate_year(year):
+    ''' fonction validation de l'année et calcul des ages et jours.'''
     y = year
     dt = datetime.date.today()
     todyear = dt.year-2000
@@ -41,10 +41,10 @@ def validate_year(year):
         try:
             y = int(year)
         except ValueError as e:
-            print(str(e))
+            # print(str(e))
             return None
-
-    print(y)
+    if datetime.date(y, 1, 1) > dt:
+        return 'trop grand !'
     if y <= todyear:
         y += 2000
     elif (y > todyear and y < 99):
