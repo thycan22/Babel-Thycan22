d27ce8697ac4ac2283c066e3ecc1411a0c6a372f
thycan22 <thycan22@gmx.fr>
2020-01-22 14:31:55 +0100

    debug

--- catalog/views.py
+++ catalog/views.py
@@ -17,14 +17,14 @@ def publication(request):
         record_list = Dewey.objects.all()
         publication_list = Publication.objects.all()
     except:
-        record = record_list = publication_list = None
+        record_list = publication_list = None
 
     context_local = {"title": "liste des publications du catalogue",
                      "description": "Publication et leurs références"}
 
     context_page = {"global": CONTEXT_GLOBAL,
                     "local": context_local,
-                    "dewey_object": record,
+                    # "dewey_object": record,
                     "dewey_object_list": record_list,
                     "publication_object_list": publication_list}
     return render(request, 'catalog/publication.html', context=context_page)
